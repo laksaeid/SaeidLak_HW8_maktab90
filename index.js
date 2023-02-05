50d444f8415b0d862f412b24c1ea6b16ac0970ee
saeid lak <laksaeidd2@gmail.com>
2023-02-05 13:31:42 +0330

    q3 solved

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const counterMaker = function () {
+  let counter = 0;
+  return function (number = 0) {
+    counter = counter + number;
+    return counter;
+  };
+};
+const counter = counterMaker();
+console.log( counter() ) // 0
+console.log( counter() ) // 0
+console.log( counter(1) ) // 1
+console.log( counter() ) // 1
+console.log( counter(3) ) // 4
+console.log( counter(7) ) // 11
+console.log( counter(-5) ) // 6
